dbe2d9b3e8bea5f2ca768201011d5fbc62377775
Gunter <mashyn.daniil@gmail.com>
2023-01-10 17:19:06 +0200

    About Section Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import About from "./components/Main/About/About";
 import Header from "./components/Header/Header";
 import Loader from "./components/Loader/Loader";
 import Hero from "./components/Main/Hero/Hero";
+
 
 export default function App() {
   return (
     <>
       <Loader />
       <Header />
-      <Hero />
+      <main>
+        <Hero />
+        <About />
+      </main>
     </>
   );
 }
---
+++ src/components/Main/About/About.jsx
@@ -0,0 +1,31 @@
+import Button from "../../../common/Button/Button";
+import "./About.css";
+
+export default function About() {
+  return (
+    <article className="about">
+      <div className="about-text">
+        <div className="row">
+          <span>СГОСТУ</span>
+          <hr />
+        </div>
+        <p>
+          Це одна з наймасовіших громадських організацій спортивного танцю
+          України. Існує понад 20 років, має представництва у більшості регіонів
+          та областей. Пріоритети - це автоматизація процесів, командна робота,
+          сучасний підхід. Наша організація - частина танцювального світу, який
+          ми робимо кращим!
+        </p>
+      </div>
+      <div className="circular">
+        <svg viewBox="0 0 100 100" className="rounded-text">
+          <path d="M 0,50 a 50,50 0 1,1 0,1 z" fill="none" id="circle" />
+          <text className="about-btn-text">
+            <textPath href="#circle">Про нас Про нас Про нас Про нас Про нас Про нас Про нас Про нас</textPath>
+          </text>
+        </svg>
+        <Button className="about-btn" buttonText="Більше" />
+      </div>
+    </article>
+  );
+}
