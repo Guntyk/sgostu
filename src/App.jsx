fb37c9ea5e5632dc1269498b7fd8a921646717e1
Gunter <mashyn.daniil@gmail.com>
2023-01-11 14:25:34 +0200

    Advantages Block Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+import Advantages from "./components/Main/Advantages/Advantages";
 import About from "./components/Main/About/About";
 import Header from "./components/Header/Header";
 import Loader from "./components/Loader/Loader";
 import Hero from "./components/Main/Hero/Hero";
-
 
 export default function App() {
   return (
@@ -12,6 +12,7 @@ export default function App() {
       <main>
         <Hero />
         <About />
+        <Advantages />
       </main>
     </>
   );
