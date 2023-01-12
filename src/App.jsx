67b833a62c7c80263e49d0cfe2feae56e61b4b40
Gunter <mashyn.daniil@gmail.com>
2023-01-12 17:34:55 +0200

    Block Uncommented

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
       <Header />
       <main>
         <Hero />
-        {/* <About /> */}
+        <About />
         <Advantages />
         {/* <NearestEvents /> */}
       </main>
