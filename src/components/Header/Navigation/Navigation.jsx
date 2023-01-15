ccc9dfcf9e21e3b60d47a5cdf1b3054c65f456e6
Kurbatishche <kurbatishche@gmail.com>
2023-01-15 19:54:12 +0200

    Update

--- src/components/Header/Navigation/Navigation.jsx
+++ src/components/Header/Navigation/Navigation.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import "./Navigation.css";
-import "./NavigationMedia.css";
 import gsap from "gsap";
 
 export default function Navigation({ openSideMenu, setOpenSideMenu }) {
