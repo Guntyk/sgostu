d92ef9a434f6774dc956e438be3cb091bef3443b
Gunter <mashyn.daniil@gmail.com>
2023-02-22 00:01:12 +0200

    Last News Corrected

--- src/components/Pages/Main/LastNews/LastNews.jsx
+++ src/components/Pages/Main/LastNews/LastNews.jsx
@@ -1,7 +1,10 @@
 import LastNewsCard from "./LastNewsCard/LastNewsCard";
+import Button from "../../../../common/Button/Button";
+import { useHistory } from "react-router-dom";
 import "./LastNews.css";
 
 export default function LastNews() {
+  const history = useHistory();
   const news = [
     {
       id: 1,
@@ -31,15 +34,25 @@ export default function LastNews() {
       <span className="news-title">Останні новини</span>
       <div className="container">
         <div className="last-news-wrapper">
-          {news.length !== 0
-            ? news
-                .slice(0, 3)
-                .map((newspaper) => (
-                  <LastNewsCard key={newspaper.id} newspaper={newspaper} />
-                ))
-            : alert("No news")}
+          {news.length !== 0 ? (
+            news
+              .slice(0, 3)
+              .map((newspaper) => (
+                <LastNewsCard key={newspaper.id} newspaper={newspaper} />
+              ))
+          ) : (
+            <span className="event-void">Новин немає</span>
+          )}
         </div>
       </div>
+      {news.length !== 0 && (
+        <Button
+          buttonText="Більше"
+          onClick={() => {
+            history.push("/");
+          }}
+        />
+      )}
     </article>
   );
 }
