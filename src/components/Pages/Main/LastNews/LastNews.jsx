caffd4f3762d2760d54d114dacc899f0a033d441
Gunter <mashyn.daniil@gmail.com>
2023-02-25 11:18:54 +0200

    Last News Section Edited, Context Fixed

--- src/components/Pages/Main/LastNews/LastNews.jsx
+++ src/components/Pages/Main/LastNews/LastNews.jsx
@@ -1,33 +1,13 @@
 import LastNewsCard from "./LastNewsCard/LastNewsCard";
 import Button from "../../../../common/Button/Button";
 import { useHistory } from "react-router-dom";
+import { Context } from "../../../..";
+import { useContext } from "react";
 import "./LastNews.css";
 
 export default function LastNews() {
   const history = useHistory();
-  const news = [
-    {
-      id: 1,
-      date: "2022-01-28",
-      title: "Вітаємо зі сріблом!",
-      description:
-        "Вероніка Мишко – Ерл Вільямсон Чемпіонат Світу дорослі 10 танців (Братислава) WDSF World Championship in Bratisla...",
-    },
-    {
-      id: 2,
-      date: "2022-02-12",
-      title: "Чемпіонат Світу серед команд...",
-      description:
-        "Команда формейшн з клубу танцювального спорту “Адажіо” м.Ірпінь посіла у фіналі 5 місце!",
-    },
-    {
-      id: 3,
-      date: "2022-03-31",
-      title: "Вітаємо Артема та Єлизавету!",
-      description:
-        "А також тренера Анну Кривенкоз першою Українською медаллю на GOC 2022!",
-    },
-  ];
+  const { news } = useContext(Context);
 
   return (
     <article className="last-news">
