9d778366cbf0a580e0879c56b8f988fccc7a6881
Kurbatishche <kurbatishche@gmail.com>
2023-02-26 19:49:33 +0200

    Footer update, News + Announcements adaptation

--- src/components/Pages/Main/Announcements/Announcements.jsx
+++ src/components/Pages/Main/Announcements/Announcements.jsx
@@ -1,7 +1,10 @@
-import AnnouncementsCard from "./AnnouncementsCard/AnnouncementsCard";
-import "./Announcements.css";
 import { useContext } from "react";
 import { Context } from "../../../..";
+import { EffectCoverflow, Pagination } from "swiper";
+import "swiper/css/pagination";
+import "swiper/css";
+import LastNewsCard from "../LastNews/LastNewsCard/LastNewsCard";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function Announcements() {
   const { announcements } = useContext(Context);
@@ -15,11 +18,35 @@ export default function Announcements() {
             ? announcements
                 .slice(0, 3)
                 .map((announcement) => (
-                  <AnnouncementsCard key={announcement.id} newspaper={announcement} />
+                  <LastNewsCard
+                    key={announcement.id}
+                    newspaper={announcement}
+                  />
                 ))
             : alert("No news")}
         </div>
       </div>
+      <Swiper
+        className="news-slider"
+        spaceBetween={-80}
+        initialSlide={1}
+        grabCursor={true}
+        slideToClickedSlide={true}
+        effect={"coverflow"}
+        pagination={true}
+        coverflowEffect={{ rotate: 0, slideShadows: false, scale: 0.8 }}
+        modules={[EffectCoverflow, Pagination]}
+      >
+        {announcements.length !== 0 ? (
+          announcements.slice(0, 3).map((newspaper) => (
+            <SwiperSlide key={newspaper.id}>
+              <LastNewsCard newspaper={newspaper} />
+            </SwiperSlide>
+          ))
+        ) : (
+          <span className="event-void">Новин немає</span>
+        )}
+      </Swiper>
     </article>
   );
 }
