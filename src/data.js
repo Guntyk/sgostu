4c59ff4d46aac5354d93fb6ae30504c02249d52c
Gunter <mashyn.daniil@gmail.com>
2023-03-06 18:27:16 +0200

    Backend Requests Added, Events Context Removed

--- src/data.js
+++ src/data.js
@@ -755,3 +755,126 @@ export const announcementsObj = `[
     "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae culpa, recusandae excepturi."
   }  
 ]`;
+
+export const dancersData = `[
+  {
+    "id": 1,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 2,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 3,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 4,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 5,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 6,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 7,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 8,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 9,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 10,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 11,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  },
+  {
+    "id": 12,
+    "name": "Іван",
+    "surname": "Іванов",
+    "thirdname": "Іванович",
+    "birthday": "2003-05-12",
+    "class": "C",
+    "club": "Diamant-Elite",
+    "organization": "СГОСТУ"
+  }
+]`;
