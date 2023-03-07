c30bbd22a1931ce71c62f069903a14bafba59d04
Gunter <mashyn.daniil@gmail.com>
2023-03-07 23:35:50 +0200

    Imports Fixes

--- src/components/Pages/Main/Main.jsx
+++ src/components/Pages/Main/Main.jsx
@@ -2,16 +2,15 @@ import { eventsSelector } from "../../../redux/events/selectors";
 import Announcements from "./Announcements/Announcements";
 import NearestEvents from "./NearestEvents/NearestEvents";
 import { getEvents } from "../../../redux/events/thunk";
+import { useDispatch, useSelector } from "react-redux";
 import Advantages from "./Advantages/Advantages";
 import Database from "./Database/Database";
 import Feedback from "./Feedback/Feedback";
 import LastNews from "./LastNews/LastNews";
-import { useDispatch, useSelector } from "react-redux";
 import Loader from "../../Loader/Loader";
 import { useEffect } from "react";
 import About from "./About/About";
 import Hero from "./Hero/Hero";
-import ClubsCard from "../ClubsCard/ClubsCard";
 
 export default function Main() {
   const events = useSelector(eventsSelector);
