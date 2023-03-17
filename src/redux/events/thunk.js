f7c78e3fb869a68a12ef28a31af4344e97718733
Gunter <mashyn.daniil@gmail.com>
2023-03-17 14:02:27 +0200

    Events Redux Fixes

--- src/redux/events/thunk.js
+++ src/redux/events/thunk.js
@@ -3,10 +3,11 @@ import { getEventsFetch } from "../../api/requests";
 
 export function getEvents() {
   return (dispatch) => {
-    getEventsFetch().then((response) => {
-      if (response) {
-        dispatch(getEventsAction(response.at(1)));
+    getEventsFetch().then(([eventsErr, events]) => {
+      if (events) {
+        dispatch(getEventsAction(events.data));
       } else {
+        console.log(eventsErr);
         alert("Getting events error");
       }
     });
