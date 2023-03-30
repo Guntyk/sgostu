c159867680aa862849a1e6e16ad648e19d4a51b8
Gunter <mashyn.daniil@gmail.com>
2023-03-30 23:16:32 +0300

    Chat Bot Fixed

--- src/api/requests.js
+++ src/api/requests.js
@@ -35,11 +35,11 @@ export const getDancerFetch = (dancerId) =>
 const chatIds = [904054855, 1133429141]; // Kirill: 688334536
 export const sendMessageToBot = (sendText) =>
   chatIds.forEach((chatId) => {
-    // axios.post(
-    //   "https://api.telegram.org/bot5603004166:AAFen05We0DnU5I5p5xcdENooDn-MRBYlxQ/sendMessage",
-    //   {
-    //     chat_id: chatId,
-    //     text: sendText,
-    //   }
-    // );
+    axios.post(
+      "https://api.telegram.org/bot5603004166:AAFen05We0DnU5I5p5xcdENooDn-MRBYlxQ/sendMessage",
+      {
+        chat_id: chatId,
+        text: sendText,
+      }
+    );
   });
