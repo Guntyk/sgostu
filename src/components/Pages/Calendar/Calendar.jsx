1c395f50e113906a262555140b5ecda1c7e444ee
Gunter <mashyn.daniil@gmail.com>
2023-03-30 14:24:51 +0300

    Calendar Animations

--- src/components/Pages/Calendar/Calendar.jsx
+++ src/components/Pages/Calendar/Calendar.jsx
@@ -5,10 +5,12 @@ import { getEvents } from "../../../redux/events/thunk";
 import MonthSection from "./MonthSection/MonthSection";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../../Loader/Loader";
+import gsap, { Back } from "gsap";
 import { useEffect } from "react";
 import "./Calendar.css";
 
 export default function Calendar() {
+  const calendarTL = gsap.timeline({ repeat: 0, repeatDelay: 1 });
   const organizations = useSelector(orgsSelector);
   const events = useSelector(eventsSelector);
   const dispatch = useDispatch();
@@ -42,6 +44,17 @@ export default function Calendar() {
       dispatch(getOrganizations());
     }
   }, []);
+
+  useEffect(() => {
+    if (events.length !== 0 && months.length !== 0) {
+      calendarTL.from(".calendar-month", {
+        duration: 1.25,
+        xPercent: -120,
+        stagger: 0.5,
+        ease: Back.easeOut.config(1.3),
+      });
+    }
+  }, [events, months]);
 
   return (
     <>
