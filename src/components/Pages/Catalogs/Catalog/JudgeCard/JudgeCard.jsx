dfeb8eadae3a3ed0abc85eea90138cd7a38eb078
Kurbatishche <kurbatishche@gmail.com>
2023-04-21 22:50:10 +0300

    catalogs adaptation

--- src/components/Pages/Catalogs/Catalog/JudgeCard/JudgeCard.jsx
+++ src/components/Pages/Catalogs/Catalog/JudgeCard/JudgeCard.jsx
@@ -1,9 +1,10 @@
 import AvatarPlaceholder from "../../../../../common/AvatarPlaceholder/AvatarPlaceholder";
 import { Link } from "react-router-dom";
+import "./JudgeCard.css";
 
 export default function JudgeCard({ judge, classes }) {
   return (
-    <div className="card-wrapper">
+    <div className="card-wrapper judge-card">
       <div className="img-wrapper">
         {judge["Foto Judges"]?.url ? (
           <img src={judge["Foto Judges"]?.url} alt="Аватар" />
@@ -11,20 +12,22 @@ export default function JudgeCard({ judge, classes }) {
           <AvatarPlaceholder />
         )}
       </div>
-      <h5 className="dancer-name">
-        {String(judge["Name Surname"]).replace(/ /g, "").trim()}
-      </h5>
-      <span className="judge-class">
-        Категорія:{" "}
-        {classes
-          .filter(
-            (judgeClass) =>
-              classes.indexOf(judgeClass) + 1 ===
-              Number(judge["Category Judge"].at(-1))
-          )
-          .at(-1)
-          .Category.trim()}
-      </span>
+      <div className="judge-wrapper">
+        <h5 className="dancer-name">
+          {String(judge["Name Surname"]).replace(/ /g, "").trim()}
+        </h5>
+        <span className="judge-class">
+          Категорія:{" "}
+          {classes
+            .filter(
+              (judgeClass) =>
+                classes.indexOf(judgeClass) + 1 ===
+                Number(judge["Category Judge"].at(-1))
+            )
+            .at(-1)
+            .Category.trim()}
+        </span>
+      </div>
       <Link className="judge-details" to={`./judges/${judge.id}`}>
         Детальніше
       </Link>
