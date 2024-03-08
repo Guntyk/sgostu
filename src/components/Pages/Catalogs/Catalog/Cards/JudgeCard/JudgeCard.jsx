ba87e582fd642658c75aac2001aced9013ce1295
Daniil Mashyn <mashyn.daniil@gmail.com>
2024-03-08 13:14:41 +0200

    Style update

--- src/components/Pages/Catalogs/Catalog/Cards/JudgeCard/JudgeCard.jsx
+++ src/components/Pages/Catalogs/Catalog/Cards/JudgeCard/JudgeCard.jsx
@@ -12,19 +12,23 @@ export default function JudgeCard({ judge, classes, screenWidth }) {
       push(`/catalogs/judges/${judge.id}`);
     }
   }
+
+  function judgeCategory() {
+    const judgeCategory = classes
+      .filter((judgeClass) => classes.indexOf(judgeClass) + 1 === Number(judge['Assigned Category Judge'][0]))
+      .at(-1)
+      .Category.trim();
+
+    return judgeCategory === 'Національна' ? 'Нац.' : judgeCategory;
+  }
+
   return (
     <div className='catalog-card judge-card' onClick={handleClick}>
       <div className='img-wrapper'>
         {judge['Foto Judges']?.url ? <img src={judge['Foto Judges']?.url} alt='Аватар' /> : <AvatarPlaceholder />}
       </div>
       <h5 className='card-name'>{judge['Name Surname']}</h5>
-      <span className='judge-class'>
-        {judge['Assigned Category Judge'] &&
-          classes
-            .filter((judgeClass) => classes.indexOf(judgeClass) + 1 === Number(judge['Assigned Category Judge'][0]))
-            .at(-1)
-            .Category.trim()}
-      </span>
+      <span className='judge-class'>{judge['Assigned Category Judge'] && judgeCategory()}</span>
       <Link className='card-details' to={`./judges/${judge.id}`}>
         {language === 'en' ? 'More' : 'Детальніше'}
       </Link>
